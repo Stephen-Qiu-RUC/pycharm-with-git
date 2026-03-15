e3c7bf7bf5fda6805e332c6ccc381607d246b053
Stephen Qiu <qiuxiaorui2006@ruc.edu.cn>
2026-03-15 11:16:36 +0800

    the first try, adding a hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+import numpy as np
+
+print("Hello, Stephen!")
+print(f"解释器位置: {sys.executable}")
+print(f"Numpy 库测试: {np.ones((2, 2))}")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pycharm-with-git!")
+
+
+if __name__ == "__main__":
+    main()
