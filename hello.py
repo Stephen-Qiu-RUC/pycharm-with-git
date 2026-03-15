5daf3284a5e40fb506b3bb5ccfd9f1368d31a781
Stephen Qiu <qiuxiaorui2006@ruc.edu.cn>
2026-03-15 12:15:26 +0800

    try to update

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 
+# An test update!!!
+print("my first github repo from git!")
 print("Hello, Stephen!")
 print(f"解释器位置: {sys.executable}")
 print(f"Numpy 库测试: {np.ones((2, 2))}")
